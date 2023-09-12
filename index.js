c8e944067123eaf8e4f5f6d05c2fbff4ab1e4ffe
dylanmatthews <djmatthews93@gmail.com>
2023-09-11 19:58:50 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet){
+  let response 
+  if (distanceInFeet <= 400) {
+    response = "This one is on me!"
+  }
+  else if (distanceInFeet > 400 && distanceInFeet < 2000) {
+    response = "That will be twenty bucks."
+  }
+  else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+    response = "I will gladly take your thirty bucks."
+  }
+  else if (distanceInFeet > 2500) {
+    response = "No can do."
+  }
+  return response
+}
+ 
+
+
+function ternaryCheckCity(cityName){
+  const cityResponse = cityName === "NYC" ? "Ok, sounds good." : "No go."
+  return cityResponse
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return  "Thank you.";
+    default:
+      return "Bye."
+  }
 }
